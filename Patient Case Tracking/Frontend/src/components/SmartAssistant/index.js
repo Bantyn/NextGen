1a09917a57ca67eaa6304c3474ca376ad99a5158
Bantyn <patelbanty1260@gmail.com>
2026-08-30 04:13:40 +0530

    fix(smart-assistant): fix default re-export syntax in SmartAssistant/index.js

--- Patient Case Tracking/Frontend/src/components/SmartAssistant/index.js
+++ Patient Case Tracking/Frontend/src/components/SmartAssistant/index.js
@@ -1,8 +1,7 @@
-export { SmartAssistant } from './SmartAssistant';
+export { SmartAssistant, default } from './SmartAssistant';
 export { SmartAssistantButton } from './SmartAssistantButton';
 export { SmartAssistantPanel } from './SmartAssistantPanel';
 export { SmartAssistantMessage } from './SmartAssistantMessage';
 export { SmartAssistantInput } from './SmartAssistantInput';
 export { SmartAssistantQuickActions } from './SmartAssistantQuickActions';
 export { SmartAssistantTyping } from './SmartAssistantTyping';
-export default SmartAssistant;
